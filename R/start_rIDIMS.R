d07cf24a2622849653b8c15329c4a254e0c36e45
felipe-mansoldo <mansoldo@micro.ufrj.br>
2024-09-22 17:55:52 -0300

    updates

--- R/start_rIDIMS.R
+++ R/start_rIDIMS.R
@@ -61,7 +61,7 @@ ui <-
                                                                selected = "sum",
                                                                inline = TRUE),
                                                   textInput("input.chr.limit", "Filter chromatogram by x% of maximum value","10"),
-                                                  checkboxInput("input.replicates", "Make 3 replicates / sample", value=TRUE),
+                                                  checkboxInput("input.replicates", "Make 3 replicates / sample", value=FALSE),
                                                   textInput("input.ppm", "ppm for grouping of mass peaks (low.res)","200"),
                                                   textInput("input.Tresh.RA", "Filter spectrum intensity by x% of maximum value","0.1"),
                                                   checkboxInput("input.replicate.filter", "Filter replicate", value=TRUE),
